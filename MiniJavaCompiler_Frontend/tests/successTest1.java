92e82ad660a1539194611edded718e75d5115b85
Lars <larspeter94@gmail.com>
2014-11-15 21:09:26 +0100

    making tests

--- MiniJavaCompiler_Frontend/tests/successTest1.java
+++ MiniJavaCompiler_Frontend/tests/successTest1.java
@@ -1,22 +1,24 @@
 class testmain {
 	public static void main(String[] args) {
-		int int_a = 1;
-		int int_b = 16;
-		int int_c = 1;
-		String string_a = "Done";
+		int int_a;
+		int int_b;
+		int int_c;
+		String string_a;
+		
+		int_a = 1;
+		int_b = 16;
+		int_c = 1;
+		string_a = "Done";
+		
+		if (string_a == "Done" && int_a == int_c) {
 
-		if (string_a == "Done" && this.int_a == this.int_c) {
+			int_a = int_a + 1;
 
-			this.int_a = int_a + 1;
-
-			while (this.int_a < this.int_b) {
+			while (int_a < int_b) {
 				int_a = int_a * 2;
-
 			}
-			System.out.print(string_a + "\n" + "int_a: " + int_a + "\nint_b: "
-					+ int_b + "\nint_c: " + int_c);
-
+			
+		System.out.print(string_a + "\n" + "int_a: " + int_a + "\nint_b: " + int_b + "\nint_c: " + int_c);
 		}
 	}
-
 }
--- MiniJavaCompiler_Frontend/tests/successTest2.java
+++ MiniJavaCompiler_Frontend/tests/successTest2.java
@@ -1,27 +1,37 @@
 class testmain {
 	public static void main(String[] args) {
-		String string_A = null;
-
-		if (A() == B()) {
+		successTest2 sT2;
+		sT2 = new sucessTest2();
+		sT2.test();
+		
+		return;
+	}
+}
+class successTest2{
+	boolean A;
+	boolean B;
+	
+	public void test() {
+		A();
+		B();
+		if (A == B) {
 			System.out.print("test went wrong");
 		} else {
 			System.out.print("test went right, but does negation work?: ");
-			if (A() == !B()) {
-				string_A = "yes it does!";
-				System.out.print(string_A);
+			if (A == !B) {
+				System.out.print("yes it does!");
 			} else {
 				System.out.print("nope go home");
 			}
 		}
-
+		return;
 	}
-
-	public boolean A() {
-		return true;
+	public void A() {
+		A = true;
+		return;
 	}
-
-	public boolean B() {
-		return false;
+	public void B() {
+		B = false;
+		return;
 	}
-
 }
